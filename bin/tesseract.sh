9ebde5dba30aab3d2c3ecbb271fcf15a245dbbdb
saiisback <karthiksaiketha@gmail.com>
2025-07-26 01:31:51 +0530

    Update tesseract wrapper to modify PATH

--- bin/tesseract.sh
+++ bin/tesseract.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+# Add our custom bin directory to the PATH for this execution
+export PATH="/var/task/bin:$PATH"
+
+# Set the Tesseract data prefix environment variable
 export TESSDATA_PREFIX=/var/task/tessdata
-exec /var/task/bin/tesseract "$@"
+
+# Now, execute tesseract. The system will find it in the path we just added.
+exec tesseract "$@"
